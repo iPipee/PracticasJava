1d7adaf47bef418f6c8640be5680887dd7081caf
iPipee <87620110+iPipee@users.noreply.github.com>
2022-06-29 18:37:34 -0400

    Terminado

--- 1ArbolAVL/src/ArbolAVL.java
+++ 1ArbolAVL/src/ArbolAVL.java
@@ -65,5 +65,55 @@ public class ArbolAVL {
     //Método para insertar AVL
     public NodoArbolAVL insertarAVL(NodoArbolAVL nuevo, NodoArbolAVL subAr){
         NodoArbolAVL nuevoPadre = subAr;
+        if(nuevo.dato<subAr.dato){
+            if(subAr.hijoIzquierdo == null){
+                subAr.hijoIzquierdo = nuevo;
+            }else{
+                subAr.hijoIzquierdo = insertarAVL(nuevo, subAr.hijoIzquierdo);
+                if((obtenerFE(subAr.hijoIzquierdo) - obtenerFE(subAr.hijoDerecho) == 2)){
+                    if(nuevo.dato < subAr.hijoIzquierdo.dato){
+                        nuevoPadre = rotacionIzquierda(subAr);
+                    }else{
+                        nuevoPadre = rotaciónDobleIzquierda(subAr);
+                    }
+                }
+            }
+        }else if(nuevo.dato > subAr.dato){
+            if(subAr.hijoDerecho == null){
+                subAr.hijoDerecho = nuevo;
+            }else{
+                subAr.hijoDerecho = insertarAVL(nuevo, subAr.hijoDerecho);
+                if((obtenerFE(subAr.hijoDerecho) - obtenerFE(subAr.hijoIzquierdo) == 2)){
+                    if(nuevo.dato > subAr.hijoDerecho.dato){
+                        nuevoPadre = rotacionDerecha(subAr);
+                    }else{
+                        nuevoPadre = rotacionDobleDerecha(subAr);
+                    }
+                }
+            }
+        }else{
+            System.out.println("Nodo duplicado");
+        }
+        //Actualizando la altura (factor de equilibrio)
+        if((subAr.hijoIzquierdo == null) && (subAr.hijoDerecho != null)){
+            subAr.fe = subAr.hijoDerecho.fe + 1;
+        }else if((subAr.hijoDerecho == null) && (subAr.hijoIzquierdo != null)){
+            subAr.fe = subAr.hijoIzquierdo.fe + 1;
+        }else{
+            subAr.fe = Math.max(obtenerFE(subAr.hijoIzquierdo), obtenerFE(subAr.hijoDerecho)) + 1;
+        }
+        return nuevoPadre;
     }
+
+    //Metodo para insertar
+    public void insertar(int d){
+        NodoArbolAVL nuevo = new NodoArbolAVL(d);
+        if(raiz == null){
+            raiz = nuevo;
+        }else{
+            raiz = insertarAVL(nuevo, raiz);
+        }
+    }
+
+    //Recorridos (Mismos que arboles binarios - Inorden - PreOrden - postOrden)
 }
